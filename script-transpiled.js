c8eec4b5934f3df1bf05581f3a9c847cda5f2042
Roberto <robertopescee@hotmail.com>
2019-05-16 23:18:44 +0200

    function constructors

--- script-transpiled.js
+++ script-transpiled.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
@@ -10,6 +10,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+*/
 /////////////////////////////////
 // Lecture: let and const
 
@@ -616,6 +617,8 @@ HINT: Use some of the ES6 features: classes, subclasses, template strings, defau
 
 */
 
+/*
+
 var Element = function Element(name, buildYear) {
     _classCallCheck(this, Element);
 
@@ -744,3 +747,5 @@ function reportStreets(s) {
 
 reportParks(allParks);
 reportStreets(allStreets);
+
+*/
